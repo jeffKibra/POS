ad8e0d51fd6f7346316e0c8fa87199b8bb58cc80
Geoffrey Ngure <kibzjeff@gmail.com>
2021-09-10 12:07:25 +0300

    firebase added

--- src/Utils/firebase.js
+++ src/Utils/firebase.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCSUm7ro7-SRJyNkhmqwVMvWADQgTm40do",
-  authDomain: "barikibarikiwa.firebaseapp.com",
-  projectId: "barikibarikiwa",
-  storageBucket: "barikibarikiwa.appspot.com",
-  messagingSenderId: "163608423571",
-  appId: "1:163608423571:web:e9239ede979ddd98fcb166",
-  measurementId: "G-CSV3KZS2SK",
+  apiKey: "AIzaSyAKNxSS9IuyNSO6_8jN2XB0WbSMIMffWck",
+  authDomain: "posimplified.firebaseapp.com",
+  projectId: "posimplified",
+  storageBucket: "posimplified.appspot.com",
+  messagingSenderId: "549075418681",
+  appId: "1:549075418681:web:c0464a2c964ca497fc57e8",
+  measurementId: "G-ZDFJQNYDYG",
 };
 
 // Initialize Firebase
